7fb45837ba26677a37ccba339c42f198e3e444ed
jtang93 <tang.jonathan93@gmail.com>
2018-12-04 13:57:46 -0500

    more gameboard work

--- avalon-online/src/App.js
+++ avalon-online/src/App.js
@@ -13,8 +13,29 @@ import Rules from './components/Rules.js'
 
 class App extends Component {
   state = {
-    currentScreen: 'Rules',
+    currentScreen: 'Gamepage',
     numberOfPlayers: 9,
+    missionVoteResults: [null, null, null, null, null],
+    voteRound: true,
+    votesArray: [],
+    missionNumber: 0
+  }
+
+  setVotesArray = (vote, id) => {
+    let arr = [...this.state.votesArray]
+    if (this.state.votesArray.includes()) {
+      let index = id - 1
+      arr[index] = vote
+      this.setState({votesArray: arr})
+    } else {
+      let count = 0
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === "yes") {
+          count++
+        }
+      }
+      
+    }
   }
 
   renderScreen = () => {
@@ -22,7 +43,12 @@ class App extends Component {
       case 'StartScreen':
         return <StartScreen />
       case 'Gamepage':
-        return <Gamepage />
+        return <Gamepage
+                numberOfPlayers={this.state.numberOfPlayers}
+                missionVoteResults={this.state.missionVoteResults}
+                voteRound={this.state.voteRound}
+                setVotesArray={this.setVotesArray}
+              />
       case 'Rules':
         return <Rules numberOfPlayers={this.state.numberOfPlayers} />
     }
